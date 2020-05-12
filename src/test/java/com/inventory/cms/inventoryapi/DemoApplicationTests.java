ad36aa8d3f2c92cb06604057b90d11b3b98f0e39
nickx7 <nickxsite@gmail.com>
2020-05-12 09:07:03 -0400

    1.1

---
+++ src/test/java/com/inventory/cms/inventoryapi/DemoApplicationTests.java
@@ -0,0 +1,14 @@
+package com.inventory.cms.inventoryapi;
+/*
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+*/
